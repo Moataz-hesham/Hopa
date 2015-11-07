38b968b618802bbf745260b7a70be68a77775a1c
NesmaBehery <nesma_wessambehery@yahoo.com>
2015-11-07 14:12:23 +0200

    this is my xml commit

--- AndroidStudioProjects/MyFirstApplication/app/src/main/java/com/example/hp/myfirstapplication/MainActivity.java
+++ AndroidStudioProjects/MyFirstApplication/app/src/main/java/com/example/hp/myfirstapplication/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
